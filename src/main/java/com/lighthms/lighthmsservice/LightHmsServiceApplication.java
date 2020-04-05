08f5a6b0f84e1f0783bb35da6e2dc9812ca69ccd
Nithin Betegeri <nithin.betegeri@navi.com>
2020-04-05 14:13:31 +0530

    Initial commit

---
+++ src/main/java/com/lighthms/lighthmsservice/LightHmsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lighthms.lighthmsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightHmsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightHmsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lighthms/lighthmsservice/LightHmsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lighthms.lighthmsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightHmsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
